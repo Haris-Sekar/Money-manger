3303a9e2ce7e8b29dad437daf5db1c1d9a0ef2c4
Bui Nam <nambk@bravebits.vn>
2021-02-18 16:18:21 +0700

    update server

--- server.js
+++ server.js
@@ -12,7 +12,18 @@ const app = express()
 
 app.use(express.json())
 
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 app.use('/api/v1/transactions', transactions)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+  
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 
 const PORT = process.env.PORT || 5000
 
