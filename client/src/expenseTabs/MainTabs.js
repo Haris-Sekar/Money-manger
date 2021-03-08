794a5306d99ea55b5497824cca6ee218e312a460
Bui Nam <nambk@bravebits.vn>
2021-03-08 17:33:45 +0700

    remove unused code

--- client/src/expenseTabs/MainTabs.js
+++ client/src/expenseTabs/MainTabs.js
@@ -1,61 +1,54 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 import { GlobalContext } from '../contexts/AppContext'
 import { Tabs, Spin } from 'antd'
-import { DollarOutlined, PoundOutlined } from '@ant-design/icons'
 import ItemList from './ItemList'
 import ItemContainer from '../itemDisplay/ItemContainer'
 import moment from 'moment'
-import styled from 'styled-components'
+
 
 export default function MainTabs({category}) {
     const { TabPane } = Tabs;
-    const { item, setItem, getTransactions, freezeBalance, setFreeBalance, keyTab, setKeyTab } = useContext(GlobalContext)
+    const { item, setItem, getTransactions, setFreeBalance, keyTab, setKeyTab } = useContext(GlobalContext)
     const allItem = item.data.filter(each => each.category === category)
     const month = moment().format("MMM")
     const year = moment().format("YYYY")
     
-   async function getMonth() {
+    function getMonth() {
         const allArray = item.data
         const thisMonth = allArray.filter(item => item.date.includes(month) && item.date.includes(year))
         setFreeBalance(true)
-     await   setItem(prev => {
+        setItem(prev => {
             return {...prev, data: thisMonth}
         })
         setKeyTab('2')
        
     }
 
-   async function getAll() {
+    function getAll() {
        setItem(prev => {
            return {...prev, Loading: true}
        })
         setFreeBalance(false)
-      await  getTransactions()
+        getTransactions()
         
         setKeyTab('1')
-    }
-
-    console.log('loading', item.Loading)
-    
+    }  
     return (
-        <Tabs  activeKey={keyTab} size="small" type='card' animated>
+        <Tabs  activeKey={keyTab} size='middle' type='card' animated>
             <TabPane
                 tab={
                     <span onClick={getAll}>
                     All Time
-                    {item.Loading && <Spin style={{paddingLeft: '5px'}}/> }
+                    {item.Loading && <Spin size='small' style={{paddingLeft: '5px'}}/> }
                     </span>
                 }
                 key="1"
                 >
-                    <>
-                   
                     <>{item.Loading? <p>loading</p>: (category === 'all')?
                         <ItemContainer />
                         :
-                        <ItemList item={allItem}/> }</>
-                   
-                   </>
+                        <ItemList item={allItem}/> }
+                    </>
                 
             </TabPane>
             <TabPane
@@ -68,24 +61,13 @@ export default function MainTabs({category}) {
                 key="2"
                
                 >
-                     <>{(category === 'all')?
+                    <>{(category === 'all')?
                         <ItemContainer />
                         :
-                        <ItemList item={allItem}/>}</>
-                    
-                
+                        <ItemList item={allItem}/>}
+                    </>
             </TabPane>
   </Tabs>
     )
 }
 
-const SpinWrapper = styled.div`
- position: relative;
-`
-
-const SpinStyle = styled.div`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`
